aba402c4c8fa68195c3d8edf9ac800f0772d5c84
Noah Kittleson <noahkittleson@gmail.com>
2016-05-27 14:56:51 -0700

    added scrolling through multiple pages of results

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,7 +1,7 @@
 var gitUser = require('./../js/gitHubApi.js').gitUser;
 
-var displayInfo = function(username, profileData) {
-  $('.showProfile').text("Here are " + username + "'s first " + profileData + " repositories:");
+var displayInfo = function(username, profileData, pageNo) {
+  $('.showProfile').text("Here is page " + pageNo + " of " + username + "'s repositories.  Showing " + profileData + " results.");
 }
 
 
@@ -9,6 +9,6 @@ $(document).ready(function(){
   $(".userSearch").submit(function(event) {
     event.preventDefault();
     var username = $("#username").val();
-    gitUser(username, displayInfo);
+    gitUser(username, displayInfo, 1);
   });
 });
--- js/gitHubApi.js
+++ js/gitHubApi.js
@@ -1,9 +1,9 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.gitUser = function(username, displayFunction) {
-  $.get('http://api.github.com/users/' + username  + '/repos?page=1&per_page=100&access_token=' + apiKey).then(function(response)  {
-    console.log(response);
-    displayFunction(username, response.length);
+exports.gitUser = function gitUser(username, displayFunction, page) {
+  $.get('http://api.github.com/users/' + username + '/repos?page=' + page + '&per_page=40&access_token=' + apiKey).then(function(response)  {
+    console.log("page " + page);
+    displayFunction(username, response.length, page);
 
     response.forEach(function(repo) {
       $('.showProfile').append("<h3>" + repo.name + "</h3>" +
@@ -18,4 +18,9 @@ exports.gitUser = function(username, displayFunction) {
   }).fail(function(error) {
     $('.showProfile').text(error.responseJSON.message);
   });
+
+  $("#more").unbind();
+  $("#more").click(function() {
+    gitUser(username, displayFunction, page+1);
+  })
 }
